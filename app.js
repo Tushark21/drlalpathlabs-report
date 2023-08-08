2a0b7ade1327ff640432e3a6a485b1e3ac321fc2
Tushar Gautam <31215069+Tushark21@users.noreply.github.com>
2023-08-08 12:07:51 +0530

    Update and rename pdf_extractor.py to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const e = require('express');
+const c = require('cors');
+
+let a = express ();
+a.use(express.json());
+a.use(cors());
+
+const port='8000';
+
+
+a.get('/0708202191786', (req,res) => {
+    res.send('hello');
+});
+
+a.listen(port, () => {
+
+});
